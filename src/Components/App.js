79b582ea115d7a9cc3049034964fd9ba2dc455ff
Jae Hui Seo <dayjaycs49@gmail.com>
2021-06-17 14:30:50 -0400

    added global styles with styled-reset and also incorporated conditional css styling on header

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import MyRouter from "./Router";
+import GlobalStyles from "./GlobalStyles";
 
 class App extends Component {
   render() {
     return (
       <>
         <MyRouter/>
+        <GlobalStyles />
       </>
     );
   }
