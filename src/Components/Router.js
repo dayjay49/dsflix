f9b2e2c1240b172a1d3a46318e0d7c6ba30b2c57
Jae Hui Seo <dayjaycs49@gmail.com>
2021-06-19 14:50:59 -0400

    DetailContainer done. Also incorporated constructor to get props from upperclass

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -14,8 +14,8 @@ const MyRouter = () => (
             <Route path="/" exact component={Home} />
             <Route path="/tv" exact component={TV} />
             <Route path="/search" component={Search} />
-            <Route path="/movie/:id" componeent={Detail} />
-            <Route path="/tv/:id" componeent={Detail} />
+            <Route path="/movie/:id" component={Detail} />
+            <Route path="/show/:id" component={Detail} />
             <Redirect from="*" to="/" />
         </Switch>
       </>
--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,14 +1,63 @@
+import { MoviesApi, TVApi } from "api";
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
 
 class DetailContainer extends React.Component{
-    state = {
-        result: null,
-        error: null,
-        loading: true
+    constructor(props) {
+        super(props);
+        const {
+            location: { pathname }
+        } = props;
+        this.state = {
+            result: null,
+            error: null,
+            loading: true,
+            isMovie: pathname.includes("/movie/")
+        }
+    }
+
+    getMovieDetail = (id) => MoviesApi.movieDetail(id);
+    getShowDetail = (id) => TVApi.showDetail(id);
+
+    async componentDidMount() {
+        const {
+            match: {
+                params: { id }
+            },
+            history: { push },
+            
+        } = this.props;
+        const { isMovie } = this.state;
+        const parsedID = parseInt(id);
+        if (isNaN(parsedID)) {
+            return push("/");
+        }
+        let result = null;
+        try {
+            if (isMovie) {
+                ({
+                    data: result
+                } = await this.getMovieDetail(parsedID));
+            } else {
+                ({
+                    data: result
+                } = await this.getShowDetail(parsedID));
+            } 
+            console.log("Result is: ", result);
+        } catch {
+            this.setState({
+                error: "Can't find anything."
+            })
+        } finally {
+            this.setState({
+                loading: false,
+                result
+            })
+        }
     }
 
     render() {
+        console.log("The state is: ", this.state);
         const { result, error, loading } = this.state;
         return (
             <DetailPresenter 
