f6b043466fd32e4747318c61d4667243edac633e
Jae Hui Seo <dayjaycs49@gmail.com>
2021-06-21 19:52:22 -0400

    DetailPresenter Part 1

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -52,7 +52,7 @@ const Poster = ({id, imageUrl, title, rating, year, isMovie = false }) => (
         <Container>
             <ImageContainer>
                 <Image bgUrl={imageUrl ? 
-                    `https://image.tmdb.org/t/p/w300/${imageUrl}` 
+                    `https://image.tmdb.org/t/p/w300${imageUrl}` 
                     : require("../assets/noPosterSmall.png")}/>
                 <Rating>
                     <span role="img" aria-label="rating">
@@ -60,7 +60,7 @@ const Poster = ({id, imageUrl, title, rating, year, isMovie = false }) => (
                     </span>{" "}{rating}/10
                 </Rating>
             </ImageContainer>
-            <Title>{title.length > 15 ? `${title.substring(0,15)}...` : title}</Title>
+            <Title>{title && title.length > 15 ? `${title.substring(0,15)}...` : title}</Title>
             <Year>{year}</Year>
         </Container>
     </Link>
